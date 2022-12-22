6afa28cd12c4f7a65a0dd58f786f3857d217682a
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2022-12-22 15:34:58 +0000

    make project shedd updated

--- bin/make_project.sh
+++ bin/make_project.sh
@@ -8,4 +8,9 @@ fi
 project_name=$1
 mkdir -p projects/${project_name}/master
 mkdir -p projects/${project_name}/build
+mkdir -p projects/${project_name}/data_augmentation
+mkdir -p projects/${project_name}/jupyer_notebook
+
+touch projects/${project_name}/data_augmentation/daug.py
+chmod 766 projects/${project_name}/data_augmentation/daug.py
 touch projects/${project_name}/README.md

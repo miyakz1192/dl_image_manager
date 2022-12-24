82adf1dae91f04927741234ce2a1b23a4fbd3288
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2022-12-24 14:16:23 +0000

    update data aug

--- lib/image_gen.py
+++ lib/image_gen.py
@@ -6,17 +6,71 @@ from tensorflow.keras.utils import array_to_img
 
 
 class DataAugmentationGenerator:
-    def __init__(self, base_img_size=(400,400), base_img_color=255, save_dir = "./"):
-        reest_count()
+    def __init__(self, image_file_path, base_img_size=(400,400), base_img_color=255, save_dir = "./"):
+        self.reest_count()
         self.base_img_size = base_img_size
         self.base_img_color = base_img_color
         self.save_dir = save_dir 
-        pass
+
+        #アップロードされた画像を読み込み
+        self.image = image.load_img(image_file_path) #表示用のイメージデータ
+        self.np_image = np.array(self.image)         #numpy配列化したもの
+        self.target_image = self.np_image[np.newaxis, :, :, :] #data augmentation化する対象                      
+
+    def show_image(self):
+        #表示画像のサイズを設定
+        #plt.figure(figsize = (32, 32))
+        #軸を表示しない
+        plt.xticks(color = "None")
+        plt.yticks(color = "None")
+        plt.tick_params(bottom = False, left = False)
+        #表示
+        plt.imshow(self.np_image)
 
     def reest_count(self):
         self.count = 0
 
-    def show_and_save(self, datagen_func, img, count_max):
+    def rotation(self):
+        #-180度〜+180度の間でランダムに回転するImageDataGeneratorを作成
+        #rotation_datagen = ImageDataGenerator(rotation_range = 180)
+        rotation_datagen = ImageDataGenerator(rotation_range = 5)
+        #画像を表示
+        self.show_and_save(rotation_datagen, self.target_image)
+
+    def height(self):
+        # 指定されたピクセル（-50〜+50）の範囲で上下にランダムに動かします。
+        #height_datagen = ImageDataGenerator(height_shift_range = 10)
+        #引数が10だと若干動かし過ぎ感がある
+        #以下は、補完時に何も指定していない
+        #height_datagen = ImageDataGenerator(height_shift_range = 5)
+        height_datagen = ImageDataGenerator(height_shift_range = 5,fill_mode="constant", cval=125)
+        self.show_and_save(height_datagen, self.target_image)
+
+    def zoom(self):
+        #0.5〜1.5の間でランダムに拡大又は縮小するImageDataGeneratorを作成
+        zoom_datagen = ImageDataGenerator(zoom_range = [0.7, 1.3],fill_mode="constant", cval=125)
+        self.show_and_save(zoom_datagen, self.target_image)
+
+    def brightness(self):
+        #画像の明るさを0.3〜0.8の間で調整（暗くする）
+        brightness_datagen = ImageDataGenerator(brightness_range = [0.1, 0.5])
+        self.show_and_save(brightness_datagen, self.target_image)
+
+    def mix(self):
+        datagen = ImageDataGenerator(rotation_range = 0.5,width_shift_range = 0.1,height_shift_range = 0.1,zoom_range = [0.7, 1.3],fill_mode="constant", cval=125)
+        self.show_and_save(datagen, self.target_image)
+
+    def mix6(self):
+        for i in range(6):
+            self.mix()
+
+    def suite(self):
+        self.height()
+        self.zoom()
+        self.brightness()
+        self.mix6()
+
+    def show_and_save(self, datagen_func, img, count_max=98):
         print("===show_and_save count=%d" % (self.count))
         #表示サイズを設定
         plt.figure(figsize = (10, 5))
@@ -38,7 +92,7 @@ class DataAugmentationGenerator:
         
           #データのセーブ処理
           #まずはベースとなる400x400画像を作る。背景は白(255,255,255)
-          base = np.full(self.base_img_size + (3), 255) #第一引数はサイズ(x,y)とともにチャネル数が必要。(x,y,c)となる。
+          base = np.full(self.base_img_size + (3,), 255) #第一引数はサイズ(x,y)とともにチャネル数が必要。(x,y,c)となる。
           #targetのサイズをbaseに埋め込む
           tx = data[0].shape[0]
           ty = data[0].shape[1]

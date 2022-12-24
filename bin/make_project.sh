d9fff0b791170c8432e89f0c1bbac4084a0a1ea5
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2022-12-24 14:53:33 +0000

    daug.py updated

--- bin/make_project.sh
+++ bin/make_project.sh
@@ -5,12 +5,13 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
+set -x
 project_name=$1
 mkdir -p projects/${project_name}/master
 mkdir -p projects/${project_name}/build
 mkdir -p projects/${project_name}/data_augmentation
 mkdir -p projects/${project_name}/jupyer_notebook
 
-touch projects/${project_name}/data_augmentation/daug.py
+cp ./sample/daug.py projects/${project_name}/data_augmentation/daug.py
 chmod 766 projects/${project_name}/data_augmentation/daug.py
 touch projects/${project_name}/README.md

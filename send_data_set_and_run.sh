be893040a55a1aecd0674ced8d4dc2dd70518f3e
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-01-20 14:02:29 +0000

    GOOD Result Obtained!

--- send_data_set_and_run.sh
+++ send_data_set_and_run.sh
@@ -12,5 +12,3 @@ pwd
 sshpass -p ${GAA_PYTORCH_SERVER_PASS}  ssh -o StrictHostKeyChecking=no ${GAA_PYTORCH_SERVER_USER}@${GAA_PYTORCH_SERVER} hostname
 sshpass -p ${GAA_PYTORCH_SERVER_PASS}  scp -o StrictHostKeyChecking=no data_set.tar.gz ${GAA_PYTORCH_SERVER_USER}@${GAA_PYTORCH_SERVER}:/tmp/
 sshpass -p ${GAA_PYTORCH_SERVER_PASS}  ssh -o StrictHostKeyChecking=no ${GAA_PYTORCH_SERVER_USER}@${GAA_PYTORCH_SERVER} "cd /home/a/pytorch_ssd/VOCdevkit/BCCD ;  rm -rf Annotations/ ImageSets/ JPEGImages/ ;  tar xvfz /tmp/data_set.tar.gz ; mv data_set/* . ; rm -rf data_set ; cd /home/a/pytorch_ssd ; ./run_learn.sh "
-
-

4ccd402de94a193941203778b7a8f730f2fb8fc4
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-01-24 15:16:35 +0000

    bin/img_classification.py bug fix and improved

--- bin/img_classification.py
+++ bin/img_classification.py
@@ -4,13 +4,15 @@ import cv2
 import os
 import argparse
 import shutil
+import uuid
 
 class ManualImageClassifier:
-    def __init__(self, input_dir=["./"], output_dir="./"):
+    def __init__(self, input_dir=["./"], output_dir="./", start_cur=0):
         self.input_dir = input_dir
         self.output_dir = output_dir
         self.in_files = []
         self.move_target_flags = []
+        self.start_cur = start_cur
 
         if os.path.isdir(self.output_dir) is not True:
             raise ValueError("output_dir = %s is not found" % (self.output_dir))
@@ -19,16 +21,22 @@ class ManualImageClassifier:
             for root, dirs, files in os.walk(top=i):
                 self.in_files += list(map(lambda x: os.path.join(root,x), files))
 
+        self.in_files.sort()
+
     def run_marking_move_target(self):
         cur = 0
         self.move_target_flags = [False for x in range(len(self.in_files))]
+
+        print("INFO: num of in files = %d" % (len(self.in_files)))
+
+        cur = self.start_cur
 
         while True:
             if cur == len(self.in_files):
                 print("END")
                 break
 
-            print("SHOWING: %s, move target=%s" % (self.in_files[cur], self.move_target_flags[cur]))
+            print("SHOWING[%d,%d]: %s, move target=%s" % (cur,float(cur)/len(self.in_files)*100, self.in_files[cur], self.move_target_flags[cur]))
             img = cv2.imread(self.in_files[cur])
             cv2.imshow("image" , img)
             print("input vim style key(h=left,j=down,k=up,l=right)")
@@ -66,7 +74,7 @@ class ManualImageClassifier:
         for i in range(len(self.in_files)):
             if self.move_target_flags[i] is True:
                 shutil.move(self.in_files[i], self.output_dir+"/")
-                print("INFO: %s => %s" % (self.in_files[i], self.output_dir+"/"))
+                print("INFO: %s => %s" % (self.in_files[i], self.output_dir+"/"+str(uuid.uuid4())))
 
 
 if __name__ == "__main__":
@@ -74,10 +82,11 @@ if __name__ == "__main__":
     
     parser.add_argument("input_dir", help="input_dir")
     parser.add_argument("output_dir", help="output_dir")
+    parser.add_argument("--start_cur", help="start number of input array index", type=int,default=0)
     
     args = parser.parse_args()
 
-    m = ManualImageClassifier([args.input_dir], args.output_dir)
+    m = ManualImageClassifier(input_dir=[args.input_dir], output_dir=args.output_dir, start_cur=args.start_cur)
     m.run_marking_move_target()
     m.print_target()
 

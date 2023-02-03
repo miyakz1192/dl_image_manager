14b38cd848ce28bf8a6ea0c379afc97b3aff80fd
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-02-03 13:55:47 +0000

    build.sh, config change support ssd,resnet34

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
+#!/usr/bin/bash 
+
+if [ $# -ne 1 ]; then
+	echo "USAGE: resnet34 or ssd"
+	exit 1
+fi
+
+type=$1
+
+if [ ${type} == "resnet34" ]; then
+	echo "resnet34"
+	cp config/dl_image_manager_config_resnet34.py lib/dl_image_manager_config.py
+elif [ ${type} == "ssd" ]; then
+	echo "ssd"
+	cp config/dl_image_manager_config_ssd.py lib/dl_image_manager_config.py
+fi
+
 ./bin/clear_project.sh
 ./bin/build_all.py
 ./bin/build_data_set.py

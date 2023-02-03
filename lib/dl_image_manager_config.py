a1ea685b2da40a432a90bc875a907ba1c20c5f8e
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-02-03 13:49:49 +0000

    remove dl_image_manager_forcing_global_base_image_size

--- lib/dl_image_manager_config.py
+++ lib/dl_image_manager_config.py
@@ -8,10 +8,3 @@ DL_IMAGE_MANAGER_FORCING_GLOBAL_BASE_IMAGE_SIZE = (-1,-1)
 
 #Edge Mode
 DL_IMAGE_MANAGER_EDGE_MODE = False
-
-
-###########################################################
-# DO NOT REMOVE BELOW
-###########################################################
-def dl_image_manager_forcing_global_base_image_size():
-    return DL_IMAGE_MANAGER_FORCING_GLOBAL_BASE_IMAGE_SIZE

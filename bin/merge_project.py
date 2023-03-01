f601be73b90d37dd73bdfbc46fd57444296d1009
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-03-01 15:11:57 +0000

    bin/merge_project.py ver 0.5

--- bin/merge_project.py
+++ bin/merge_project.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-import glob
+import sys
+sys.path.append("./lib/")
 
+import glob
+import shutil
+
+from get_current_process_user_home_dir import *
+from dl_image_manager_constants import *
+from xml_lib import * 
 
 class ProjectMerger:
     def __init__(self, src_prj=None, dst_prj=None, projects_dir=None):
@@ -16,58 +23,61 @@ class ProjectMerger:
         #load configuration of src_prj and dst_prj from files
         pass
 
-    def __project_build_dir_name(self,project_name):
+    def __prj_build_dir_name(self,project_name):
         return self.projects_dir + "/" + project_name + "/build"
 
     def __count_jpg_files_in_project(self, project_name):
-        return self.__count_jpg_files(self.__project_build_dir_name(project_name))
+        return self.__count_jpg_files(self.__prj_build_dir_name(project_name))
 
     def __prj_file_name_prefix(self, project_name):
         return "%s_" % project_name
 
+    def __prj_jpg_file_path(self, project_name, count):
+        return self.__prj_build_dir_name(project_name) + "/" + self.__prj_file_name_prefix(project_name) + str(count) + ".jpg"
+
+    def __prj_annotation_file_path(self, project_name, count):
+        return self.__prj_build_dir_name(project_name) + "/" + self.__prj_file_name_prefix(project_name) + str(count) + ".xml"
+
     def __transform_and_copy_annotation_file(self, project_name, src_count , dst_count):
-        import pdb
-        pdb.set_trace()
-        src_anno_file_name = self.__prj_file_name_prefix(project_name) + str(src_count) + ".xml"
-        dst_anno_file_name = self.__prj_file_name_prefix(self.dst_prj) + str(dst_count) + ".xml"
+        src_anno_path = self.__prj_annotation_file_path(project_name, src_count)
+        dst_anno_path = self.__prj_annotation_file_path(self.dst_prj, dst_count)
 
-        data = self.__get_annotation_data_and_rewrite(src_anno_file_name)
+        xmlo = XMLOperator()
+        xmlo.read(src_anno_path)
+        #filename tag
+        xmlo.rewrite("filename", os.path.basename(self.__prj_jpg_file_path(self.dst_prj, dst_count)))
+        #object, name tag
+        xmlo.rewrite("name", self.dst_prj)
+        xmlo.write(dst_anno_path)
 
-        self.__save_xml_data_as(data, dst_anno_file_name)
+        print("INFO: copy %s to %s" % (src_anno_path, dst_anno_path))
 
-        print(dst_anno_file_name)
+    def __copy_jpg_file(self, project_name, src_count, dst_count):
+        src_jpg_path = self.__prj_jpg_file_path(project_name, src_count)
+        dst_jpg_path = self.__prj_jpg_file_path(self.dst_prj, dst_count)
 
-    def __get_annotation_data_and_rewrite(self, file_name):
-        pass
-
-    def __save_xml_data_as(self, data, dst_anno_file_name):
-        pass
-
-    def __copy_jpg_file(self, project_name, suffix_count):
-        pass
+        shutil.copyfile(src_jpg_path, dst_jpg_path)
+        print("INFO: copy %s to %s" % (src_jpg_path, dst_jpg_path))
 
     def __merge_one_prj(self, target_prj, next_count):
         target_prj_jpg_file_count = self.__count_jpg_files_in_project(target_prj)
-
+        print("INFO: source project of %s jpg files count=%d" % (target_prj, target_prj_jpg_file_count))
 
         for i in range(target_prj_jpg_file_count):
             self.__transform_and_copy_annotation_file(target_prj, i, next_count + i)
-            self.__copy_jpg_file(target_prj,i)
+            self.__copy_jpg_file(target_prj,i, next_count + i)
 
         return next_count
 
     def merge(self):
-        #FIXME: good path def like build_data_set.py
         self.dst_prj_jpg_file_count = self.__count_jpg_files(self.projects_dir + "/" + self.dst_prj + "/build")
 
-        print("INFO: %s jpg files count=%d" % (self.dst_prj, self.dst_prj_jpg_file_count))
+        print("INFO: destination project of %s jpg files count=%d" % (self.dst_prj, self.dst_prj_jpg_file_count))
 
         next_count = self.dst_prj_jpg_file_count
         for target_prj in self.src_prj:
             next_count = self.__merge_one_prj(target_prj, next_count)
 
-
 if __name__ == "__main__":
-    print("main!")
     pm = ProjectMerger(src_prj=["closebcow"], dst_prj="close", projects_dir="./projects")
     pm.merge()

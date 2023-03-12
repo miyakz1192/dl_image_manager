62dcfe431561871c732fd8a969cb350a270d335f
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-03-12 15:01:51 +0000

    config/dl_image_manager_config_resnet34.py definition precicely

--- config/dl_image_manager_config_resnet34.py
+++ config/dl_image_manager_config_resnet34.py
@@ -7,7 +7,8 @@ DL_IMAGE_MANAGER_FORCING_GLOBAL_BASE_IMAGE_SIZE = (-1,-1)
 #DL_IMAGE_MANAGER_FORCING_GLOBAL_BASE_IMAGE_SIZE = (400,400)
 
 #ResNet34
-DL_IMAGE_MANAGER_MERGE_CONFIG=(["closebcow", "closegb", "closewcobfat", "closewcolg"], "close", "./projects")
+DL_IMAGE_MANAGER_MERGE_CONFIG_INIT_SRC= ["closebcow", "closegb", "closewcobfat", "closewcolg"]
+DL_IMAGE_MANAGER_MERGE_CONFIG=(DL_IMAGE_MANAGER_MERGE_CONFIG_INIT_SRC, "close", "./projects")
 
 #Edge Mode
 DL_IMAGE_MANAGER_EDGE_MODE = False
